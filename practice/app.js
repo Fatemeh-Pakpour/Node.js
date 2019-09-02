fa903ec3a5ce983469371c354c3163761983aa32
Fatemeh Pakpour <fatemehpakpour64@gmail.com>
2019-09-02 21:16:21 +0200

    modified the data callback to concatenate the stream

--- practice/app.js
+++ practice/app.js
@@ -1,21 +1,45 @@
-console.log("Hello World!");
-console.error("ops, sth wrong");
-console.dir({
-    name: "FAFA",
-    age:23
-});
+// console.log("Hello World!");
+// console.error("ops, sth wrong");
+// console.dir({
+//     name: "FAFA",
+//     age:23
+// });
 // https request
-const https = require('https');
-const url = 'https://encrypted.google.com/';
+// const https = require('https');
+// const url = 'https://encrypted.google.com/';
 
-https.get(url, (response) => {
-  console.log('statusCode:', response.statusCode);
-  console.log('headers:', response.headers);
+// https.get(url, (response) => {
+//   console.log('statusCode:', response.statusCode);
+//   console.log('headers:', response.headers);
 
-  response.on('data', (d) => {
-    process.stdout.write(d);
-  });
+//   response.on('data', (d) => {
+//     process.stdout.write(d);
+//   });
 
-}).on('error', (error) => {
-  console.error(error);
+// }).on('error', (error) => {
+//   console.error(error);
+// });
+
+  // treehouse exercise
+  
+
+
+// 
+const https = require("https");
+const request = https.get("https://teamtreehouse.com/chalkers.json", response => {
+    let responseBody = "";
+
+    response.on("data", dataChunk => {
+       responseBody += dataChunk,toString();
+
+    });
+
+    response.on("end", () => {
+        console.log(responseBody);
+    });
+
+});
+
+request.on("error", error => {
+    console.error(error.message);
 });
