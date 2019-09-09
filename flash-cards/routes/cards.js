09bce6a6b2aa4e99004236f6d2cab2c7ebf31dce
Fatemeh Pakpour <fatemehpakpour64@gmail.com>
2019-09-09 22:45:12 +0200

    added a query string

--- flash-cards/routes/cards.js
+++ flash-cards/routes/cards.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {data}= require('../data/flashCardData.json');
+const { data } = require("../data/flashCardData.json");
+const { cards } = data;
 
-router.get('/:id', (req, res)=>{
-    const id = req.params.id;
-    const num = Number(id);
-    res.json(data.cards[num]);
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const { side } = req.query;
+  res.json(`${cards[id][side]}`);
+
 });
-
-
 
 module.exports = router;
