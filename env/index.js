cd057259b5ed9586e668282ee13114a7dd957232
Fatemeh Pakpour <fatemehpakpour64@gmail.com>
2019-09-23 11:05:36 +0200

    added the gitignore file

--- env/index.js
+++ env/index.js
@@ -1,20 +1,20 @@
 const mysql = require("mysql");
-// require("dotenv").config();
-// const {
-//  USER: user,
-//  PASSWORD: password,
-//  HOST: host,
-//  DATABASE: database,
-//  PORT: port
-// } = process.env;
+require("dotenv").config();
+const {
+ USER: user,
+ PASSWORD: password,
+ HOST: host,
+ DATABASE: database,
+ PORT: port
+} = process.env;
 const db = mysql.createConnection({
  user:"root",
- password:"fat?/1568?/pak",
+ password:"1568",
  host:"localhost",
  database:"phonebook"
 });
 db.connect();
-// db.end();
+ db.end();
 // db.getConnection((err,connection)=>{
 //     console.log({
 //         err,connection
